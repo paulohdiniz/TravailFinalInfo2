ed40d2fafb1900cb4e557f5efa5a7bb40776238c
Paulo <ph.diniz@usp.br>
2022-11-14 09:06:05 +0100

    server running

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/paulohd/Documents/materias ecm/Info2 - Data Driven Programming/TravailFinalSite/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
